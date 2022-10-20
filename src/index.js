5aa191375f4892853dbb1659920767d0d066bff7
NgoDacLoi <91691659+NgoDacLoi@users.noreply.github.com>
2022-10-20 14:36:17 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,40 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+/*root.render(
   <p>
       hello, <strong> JSK</strong>
   </p>
+)*/
+root.render(
+        <div>
+            <button />
+             <code />
+             <input />
+            <label />
+             <p />
+             <pre />
+             <select />
+             <table />
+             <ul />
+        </div>
+)
+
+root.render(
+    <section>
+      <header>
+        <h1>Header</h1>
+        </header>  
+        <nav>
+          <a href='item'>Nav Item</a>
+        </nav>
+        <main>
+          <p> the main content ...</p>
+        </main>
+        <footer>
+          <small>&copy; 2021</small>
+        </footer>
+    </section>
 )
 
 // If you want to start measuring performance in your app, pass a function
