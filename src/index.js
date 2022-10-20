8f9e9990721c3147141d831d4db2c90a564e88bd
NgoDacLoi <91691659+NgoDacLoi@users.noreply.github.com>
2022-10-20 15:39:13 +0700

    third commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import MySection from "./Mysection"
+import Mybutton from './MyButton';
+import MyComponent from './MyComponent';
+import MyComponent2 from './Mycomponent2';
+
+const array=["first","second","third"];
+const object={
+  first: 1,
+  second: 2,
+  third: 3,
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-/*root.render(
+root.render(
   <p>
       hello, <strong> JSK</strong>
   </p>
-)*/
+)
 root.render(
         <div>
             <button />
@@ -40,6 +51,38 @@ root.render(
         </footer>
     </section>
 )
+
+root.render(
+  <MySection>
+    <Mybutton>My Button Text</Mybutton>
+  </MySection>
+)
+
+
+root.render(
+  <section>
+    <h1>Array</h1>
+    <ul>
+      {array.map((i) =>(
+        <li key={i}>{i}</li>
+      ))}
+    </ul>
+
+    <h1>Object</h1>
+    <ul>
+      {Object.keys(object).map((i) => (
+        <li key={i}>
+          <strong>{i}: </strong>
+          {object[i]}
+        </li>
+      ))}
+    </ul>
+  </section>
+)
+
+root.render(<MyComponent />)
+
+root.render(<MyComponent2 />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
